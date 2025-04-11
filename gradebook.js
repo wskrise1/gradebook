eac6b0204156120ec4352e02b9ea271272eca9e7
wskrise1 <wskrise1@my.waketech.edu>
2025-04-11 09:57:25 -0400

    Update and rename README.md to gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,19 @@
+# Gradebook
+<!DOCTYPE html>
+<html>
+</head>
+<body>
+  <h1>Gradebook</h1>
+  <table id="gradebook">
+    <tr>
+      <th>Student Name</th>
+      <th>Assignment 1</th>
+      <th>Assignment 2</th>
+      <th>Assignment 3</th>
+    </tr>
+  </table>
+</body>
+</html>
+
+
+
